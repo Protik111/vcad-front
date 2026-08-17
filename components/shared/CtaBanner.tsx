aaa1548745495f56a4c8005782704c3cbdf60981
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-08-17 20:30:18 +0600

    feat: cta

--- components/shared/CtaBanner.tsx
+++ components/shared/CtaBanner.tsx
@@ -6,15 +6,15 @@ import LogoMark from "@/components/ui/LogoMark";
  */
 export default function CtaBanner() {
   return (
-    <section className="py-20 sm:py-28">
+    <section className="py-30 sm:py-38 bg-[url('/images/cta-background.png')] bg-right bg-no-repeat py-20 sm:py-28 bg-size-[605px]">
       <Container>
-        <LogoMark size={44} />
+        <LogoMark size={102} />
         <p className="mt-8 max-w-4xl text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-section">
           Get creative and{" "}
-          <span className="bg-gradient-to-r from-pink to-magenta bg-clip-text text-transparent">
-            turn your passion
+          <span className="text-magenta">
+            turn <br /> your passion
           </span>{" "}
-          for the Arts into a rewarding career.
+          for the Arts into a <br /> rewarding career.
         </p>
       </Container>
     </section>
